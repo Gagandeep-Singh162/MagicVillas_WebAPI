bd560ae9a614015497174f999a71ae27b29c0005
Gagandeep Singh <gagan162000@gmail.com>
2025-03-06 14:44:37 +0530

    Added a simple API + DTO

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,20 @@
+﻿using MagicVilla_VillaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class VillaAPIController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa {Id = 1, Name ="Pool" },
+                new Villa {Id = 2, Name ="Beach" }
+            };
+        }  
+    }
+}
