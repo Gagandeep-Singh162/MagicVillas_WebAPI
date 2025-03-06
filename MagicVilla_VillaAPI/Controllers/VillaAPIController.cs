044d555f8a3966586e36ad2ccd78300659563761
Gagandeep Singh <gagan162000@gmail.com>
2025-03-06 15:15:15 +0530

    Added response types attribute

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,20 +1,39 @@
-﻿using MagicVilla_VillaAPI.Models;
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    public class VillaAPIController : Controller
+    public class VillaAPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<Villa> GetVillas()
+        [ProducesResponseType(statusCode: StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<VillaDto>> GetVillas()
         {
-            return new List<Villa>
-            {
-                new Villa {Id = 1, Name ="Pool" },
-                new Villa {Id = 2, Name ="Beach" }
-            };
-        }  
+            return Ok(VillaStore.villaList); 
+        }
+
+        [HttpGet("id")]
+        [ProducesResponseType(statusCode:StatusCodes.Status200OK)]
+        [ProducesResponseType(statusCode: StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(statusCode:StatusCodes.Status404NotFound)]
+        public ActionResult<VillaDto?> GetVilla(int id)
+        {
+            if (id == 0) {
+                return BadRequest();
+            }
+
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+
+
+            if (villa == null) { 
+                return NotFound();
+            }
+
+            return Ok(villa);
+        }
     }
 }
